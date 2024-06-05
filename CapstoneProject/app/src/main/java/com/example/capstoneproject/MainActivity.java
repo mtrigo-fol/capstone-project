f6479913c9cd78de75fb1aab2ff5723f92d3c25f
Murilo Trigo <m_trigo@fanshaweonline.ca>
2024-06-05 12:22:06 -0400

    #1 Starts implementing task creation

--- CapstoneProject/app/src/main/java/com/example/capstoneproject/MainActivity.java
+++ CapstoneProject/app/src/main/java/com/example/capstoneproject/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
+    private int taskCreationVariable = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
